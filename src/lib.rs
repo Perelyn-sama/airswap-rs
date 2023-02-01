02cb8633b4917c608f64c81213a9f28740867195
Perelyn <pisckid344@gmail.com>
2023-02-01 12:00:43 +0100

    remove bindings mod;add contracts mod; add comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-pub mod bindings;
+//! # airswap-rs
+//!
+//! Unofficial Rust SDK library for Airswap smart contracts.
+
 pub mod common;
 pub mod pool;
 pub mod registry;
 pub mod staking;
 pub mod swap_erc20;
 pub mod wrapper;
+pub mod contracts;
