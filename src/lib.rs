f1028655f7df66cf6394c134f966fbb4a8b06a5a
Perelyn <pisckid344@gmail.com>
2023-02-26 06:53:47 +0100

    add prelude mod

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,18 @@ pub mod staking;
 pub mod swap_erc20;
 pub mod utils;
 pub mod wrapper;
+
+pub mod prelude {
+    pub use super::{
+        common::constants::ADDRESS_ZERO,
+        pool::Pool,
+        registry::Registry,
+        staking::Staking,
+        swap_erc20::SwapERC20,
+        utils::{pool::create_claim, swap::create_order},
+        wrapper::Wrapper
+    };
+
+    #[cfg(feature = "addresses")]
+    pub use super::contracts::addresses::{address, contract, try_address, try_contract};
+}
