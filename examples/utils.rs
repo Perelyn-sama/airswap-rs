202459a1a1c855a6c9bba6dc221b67ebe86832f8
Perelyn <pisckid344@gmail.com>
2023-03-29 09:39:30 +0100

    refactored println

--- examples/utils.rs
+++ examples/utils.rs
@@ -7,7 +7,7 @@ async fn main() -> Result<()> {
     // Create order
     let order = create_order(None, None, None, None, None, None, true);
     println!(
-        "Default Order: Nonce {}, Expiry {}, Protocol Fee {}, Signer {}, Sender {}, Affiliate {}",
+        "Default Order: Nonce {},\n Expiry {},\n Protocol Fee {},\n Signer {},\n Sender {},\n Affiliate {}",
         order.nonce,
         order.expiry,
         order.protocol_fee,
@@ -19,7 +19,7 @@ async fn main() -> Result<()> {
     // Create claim
     let claim = create_claim(None, None, None, None, true);
     println!(
-        "Default Claim: Nonce {}, Expiry {}, Participant {}, Score {}",
+        "Default Claim: Nonce {},\n Expiry {},\n Participant {},\n Score {}",
         claim.nonce, claim.expiry, claim.participant, claim.score
     );
 
